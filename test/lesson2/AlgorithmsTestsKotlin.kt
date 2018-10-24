4a9b027edec016e358273649d728c8c976425e69
crazytosser <666_mickhail@mail.ru>
2018-10-24 11:12:59 +0300

    second commit

--- test/lesson2/AlgorithmsTestsKotlin.kt
+++ test/lesson2/AlgorithmsTestsKotlin.kt
@@ -41,4 +41,24 @@ class AlgorithmsTestsKotlin : AbstractAlgorithmsTests() {
     fun testBaldaSearcher() {
         baldaSearcher { inputName, words -> baldaSearcher(inputName, words) }
     }
+
+    @Test
+    @Tag("Normal")
+    fun testLongest() {
+        assertEquals("41SAw343", lesson2.longestCommonSubstring(
+                """
+                    5441SAw343sssaweq!@#sdxc
+                """.trimIndent(),
+                """
+                    trWERAS12341SAw343
+                """.trimIndent()
+        ))
+    }
+
+    @Test
+    @Tag("Hard")
+    fun testBalda() {
+        assertEquals(setOf("ЛЕД", "ЕДА", "НИНА", "ЗАНОЗА", "ЛИНЗА"), baldaSearcher("input/balda_in2.txt",
+                setOf("ЛЕД", "ЕДА", "НИНА", "ЗАНОЗА", "ЛИНЗА", "ЕГА", "СМЫСЛ")))
+    }
 }
